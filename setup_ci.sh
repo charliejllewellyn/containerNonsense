e4720c4d15d25240e17c7e6a121acbe558c0f427
Charles Llewellyn <charlie.j.llewellyn@gmail.com>
2017-06-22 21:31:26 +0100

    Add jenkins setup

--- setup_ci.sh
+++ setup_ci.sh
@@ -6,3 +6,7 @@ while !(oc logs -f bc/containernonsense | grep 'Push successful' -q); do
   echo waiting for build to complete
   sleep 10
 done
+
+oc delete template/jenkins-persistent
+oc create -f jenkins-persistent-template.json
+oc new-app jenkins-persistent
