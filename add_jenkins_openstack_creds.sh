97936d22e3f6fe9be2e0755c5daa515cd6073036
Charles Llewellyn <charlie.j.llewellyn@gmail.com>
2017-06-22 20:59:32 +0100

    Add jenkins file

---
+++ add_jenkins_openstack_creds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo enter Jenkins username:
+read jenkins_user
+echo enter Jenkins API token:
+read jenkins_token
+
+openstack_password=$(export | grep OS_PASSWORD | awk -F= '{print $2}' | sed "s/'//g")
+openstack_username=$(export | grep OS_USERNAME | awk -F= '{print $2}')
+
+curl -X POST "https://$jenkins_user:$jenkins_token@jenkins-continuous-integration.apps.customer1.flowersandsausages.co.uk/credentials/store/system/domain/_/createCredentials" -k \
+--data-urlencode "json={
+  \"\": \"0\",
+  \"credentials\": {
+    \"scope\": \"GLOBAL\",
+    \"id\": \"openstackCreds\",
+    \"username\": $openstack_username,
+    \"password\": $openstack_password,
+    \"description\": \"OpenStack credentials for heat deployment\",
+    \"\$class\": \"com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl\"
+  }
+}"
